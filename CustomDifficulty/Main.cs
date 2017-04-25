3cc5a4f4e20a92199fd037293ed99f8077714729
Gyrodrill <rshyys@126.com>
2017-04-25 14:21:02 +0800

    add support to misc equips (not tested or compiled)

--- CustomDifficulty/Main.cs
+++ CustomDifficulty/Main.cs
@@ -3,6 +3,7 @@ using TerrariaApi.Server;
 using Newtonsoft.Json;
 using System.IO;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TShockAPI;
@@ -24,28 +25,26 @@ namespace Koishi
 		public CDMain(Main game) : base(game)
 		{
 		}
-
 		public override void Initialize()
 		{
 			ServerApi.Hooks.NetGetData.Register(this, NetHooks_GetData);
 			ReadConfig(configPath, new Config(), ref config);
 			Commands.ChatCommands.Add(new Command("Koishi.CustomDifficulty.reload", CDReload, "cdr", "cdreload"));
 		}
-
 		private void CDReload(CommandArgs args)
 		{
 			ReadConfig(configPath, new Config(), ref config);
 		}
-
-		public static int[] GetRandomInRange(int Count, int Max)
+		public static int[] GetRandomInRange(int Count, int Max, int[] Seq = null)
 		{
-			//var rand = new Random();
-			var Seq = new int[Max];
-			for (int i = 0; i < Max; i++)
+			if (Seq == null)
 			{
-				Seq[i] = i;
+				Seq = new int[Max];
+				for (int i = 0; i < Max; i++)
+				{
+					Seq[i] = i;
+				}
 			}
-
 			int end = Seq.Length;
 			int[] ret = new int[Count];
 			for (int i = 0; i < Count; i++)
@@ -56,10 +55,22 @@ namespace Koishi
 				Seq[r] = Seq[end];
 				Seq[end] = ret[i];
 			}
-
 			return ret;
 		}
-
+		public static T[] GetRandomInArray<T>(int Count, T[] Seq)
+		{
+			int end = Seq.Length;
+			T[] ret = new T[Count];
+			for (int i = 0; i < Count; i++)
+			{
+				int r = Main.rand.Next(0, end);
+				end--;
+				ret[i] = Seq[r];
+				Seq[r] = Seq[end];
+				Seq[end] = ret[i];
+			}
+			return ret;
+		}
 		private void NetHooks_GetData(GetDataEventArgs args)
 		{
 			if (args.MsgID == PacketTypes.PlayerDeathV2)
@@ -77,166 +88,17 @@ namespace Koishi
 				{
 					case 1:
 						{
-							{
-								int inventoryCount = 0;
-								for (int i = 0; i < p.inventory.Length; i++)
-								{
-									var item = p.inventory[i];
-									if (!item.active || item.stack == 0 || item.type == 0)
-									{
-										continue;
-									}
-
-									inventoryCount++;
-								}
-
-								List<int> DropSeq = new List<int> { };
-								bool DropAll = false;
-								if (inventoryCount <= config.ItemDropAmount)
-								{
-									DropAll = true;
-								}
-								else
-								{
-									DropSeq = GetRandomInRange(config.ItemDropAmount, inventoryCount).ToList();
-								}
-
-								int inventortIndex = 0;
-								for (int i = 0; i < p.inventory.Length; i++)
-								{
-									var item = p.inventory[i];
-									if (!item.active || item.stack == 0 || item.type == 0)
-									{
-										continue;
-									}
-
-									if (DropAll || DropSeq.Contains(inventortIndex))
-									{
-										notice.Append(TShock.Utils.ItemTag(p.inventory[i]));
-										if (config.Vanish)
-										{
-											p.inventory[i].SetDefaults(0);
-											NetMessage.SendData(5, -1, -1, null, playerID, i, p.inventory[i].prefix);
-										}
-										else
-										{
-											Item.NewItem(p.position, item.width, item.height, item.type, item.stack);
-											p.inventory[i].SetDefaults(0);
-											NetMessage.SendData(5, -1, -1, null, playerID, i, p.inventory[i].prefix);
-										}
-									}
-
-									inventortIndex++;
-								}
-							}
-							{
-								int armorCount = 0;
-								for (int i = 0; i < p.armor.Length; i++)
-								{
-									var item = p.armor[i];
-									if (!item.active || item.stack == 0 || item.type == 0)
-									{
-										continue;
-									}
-
-									armorCount++;
-								}
-
-								List<int> DropSeq = new List<int> { };
-								bool DropAll = false;
-								if (armorCount <= config.ItemDropAmount)
-								{
-									DropAll = true;
-								}
-								else
-								{
-									DropSeq = GetRandomInRange(config.ItemDropAmount, armorCount).ToList();
-								}
-
-								int armorIndex = 0;
-								for (int i = 0; i < p.armor.Length; i++)
-								{
-									var item = p.armor[i];
-									if (!item.active || item.stack == 0 || item.type == 0)
-									{
-										continue;
-									}
-
-									if (DropAll || DropSeq.Contains(armorIndex))
-									{
-										notice.Append(TShock.Utils.ItemTag(p.armor[i]));
-										if (config.Vanish)
-										{
-											p.armor[i].SetDefaults(0);
-											NetMessage.SendData(5, -1, -1, null, playerID, i + 59, p.armor[i].prefix);
-										}
-										else
-										{
-											Item.NewItem(p.position, item.width, item.height, item.type, item.stack);
-											p.armor[i].SetDefaults(0);
-											NetMessage.SendData(5, -1, -1, null, playerID, i + 59, p.armor[i].prefix);
-										}
-									}
-
-									armorIndex++;
-								}
-							}
-
+							Drop1AndAppendNotice(p, playerID, p.inventory, config.ItemDropAmount, notice, 0);
+							Drop1AndAppendNotice(p, playerID, p.armor, config.EquipDropAmount, notice, 58);
+							Drop1AndAppendNotice(p, playerID, p.miscEquips, config.MiscDropAmount, notice, 88);
 							break;
 						}
 
 					case 2:
 						{
-							for (int i = 0; i < p.inventory.Length; i++)
-							{
-								var item = p.inventory[i];
-								if (!item.active || item.stack == 0 || item.type == 0)
-								{
-									continue;
-								}
-
-								if (Main.rand.Next(100) < config.RandomDropRate)
-								{
-									if (config.Vanish)
-									{
-										notice.Append(TShock.Utils.ItemTag(p.inventory[i]));
-										p.inventory[i].SetDefaults(0);
-										NetMessage.SendData(5, -1, -1, null, playerID, i, p.inventory[i].prefix);
-									}
-									else
-									{
-										Item.NewItem(p.position, item.width, item.height, item.type, item.stack);
-										p.inventory[i].SetDefaults(0);
-										NetMessage.SendData(5, -1, -1, null, playerID, i, p.inventory[i].prefix);
-									}
-								}
-							}
-
-							for (int i = 0; i < p.armor.Length; i++)
-							{
-								var item = p.armor[i];
-								if (!item.active || item.stack == 0 || item.type == 0)
-								{
-									continue;
-								}
-
-								if (Main.rand.Next(100) < config.RandomDropRate)
-								{
-									if (config.Vanish)
-									{
-										notice.Append(TShock.Utils.ItemTag(p.armor[i]));
-										p.armor[i].SetDefaults(0);
-										NetMessage.SendData(5, -1, -1, null, playerID, i + 59, p.armor[i].prefix);
-									}
-									else
-									{
-										Item.NewItem(p.position, item.width, item.height, item.type, item.stack);
-										p.armor[i].SetDefaults(0);
-										NetMessage.SendData(5, -1, -1, null, playerID, i + 59, p.armor[i].prefix);
-									}
-								}
-							}
-
+							Drop2AndAppendNotice(p, playerID, p.inventory, config.ItemDropAmount, notice, 0);
+							Drop2AndAppendNotice(p, playerID, p.armor, config.EquipDropAmount, notice, 58);
+							Drop2AndAppendNotice(p, playerID, p.miscEquips, config.MiscDropAmount, notice, 88);
 							break;
 						}
 				}
@@ -248,7 +110,38 @@ namespace Koishi
 				}
 			}
 		}
-
+		public void Drop1AndAppendNotice(Player p, int playerID, Item[] inv, int count, StringBuilder notice, int os)
+		{
+			var invs = inv.Where(item => item.active && item.stack != 0 && item.type != 0);
+			var DropSeq = GetRandomInArray(config.ItemDropAmount, invs.ToArray());
+			var pil = inv.ToList();
+			while (DropSeq.Any())
+			{
+				var d = pil.IndexOf(DropSeq[0]);
+				notice.Append(TShock.Utils.ItemTag(inv[d]));
+				DropItem(p, playerID, pil[d], os + d);
+			}
+		}
+		public void Drop2AndAppendNotice(Player p, int playerID, Item[] inv, int rate, StringBuilder notice, int os)
+		{
+			for (int i = 0; i < inv.Length; i++)
+			{
+				if (Main.rand.Next(100) < config.RandomDropRate)
+				{
+					notice.Append(TShock.Utils.ItemTag(inv[i]));
+					DropItem(p, playerID, inv[i], os + i);
+				}
+			}
+		}
+		public void DropItem(Player p, int playerID, Item i, int itemID)
+		{
+			if (!config.Vanish)
+			{
+				Item.NewItem(p.position, i.width, i.height, i.type, i.stack);
+			}
+			i.TurnToAir();
+			NetMessage.SendData(5, -1, -1, null, playerID, itemID, i.prefix);
+		}
 		public static void ReadConfig<ConfigType>(string path, ConfigType defaultConfig, ref ConfigType config)
 		{
 			if (!File.Exists(path))
@@ -263,11 +156,11 @@ namespace Koishi
 			}
 		}
 	}
-
 	public class Config
 	{
 		public int ItemDropAmount = 2;
 		public int EquipDropAmount = 1;
+		public int MiscDropAmount = 1;
 		public int RandomDropRate = 10;
 		public int UseDropWay = 1;
 		public bool Vanish = false;
